8d4b424236dc6edfeb41be7cd4f741400d42ca38
marioldurao <marioldurao@gmail.com>
2017-06-19 23:43:51 +0100

    login social working

---
+++ Projec1/resources/views/layouts/partials/_header.blade.php
@@ -0,0 +1,10 @@
+ <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="intro-text">
+                <div class="intro-lead-in">Climbing Wearable</div>
+                <div class="intro-heading">improve your basic climbing skills</div>
+                <a href="#services" class="page-scroll btn btn-xl">Climbing</a>
+            </div>
+        </div>
+    </header>
